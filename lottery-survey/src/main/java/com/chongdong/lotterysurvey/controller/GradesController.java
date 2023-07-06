88d2cd95dd59fb581ded0146e69428ef7df7c378
Pip190 <wjk152153431@outlook.com>
2023-07-06 11:12:49 +0800

    新增：启动类@MapperScan扫描注、controller层

---
+++ lottery-survey/src/main/java/com/chongdong/lotterysurvey/controller/GradesController.java
@@ -0,0 +1,12 @@
+package com.chongdong.lotterysurvey.controller;
+
+/**
+ * Description: 个人成绩（答题成绩）
+ * Author: wo
+ * Created: 2023/7/6 10:43
+ * Modified By: wo
+ * Last Modified: 2023/7/6 10:43
+ */
+public class GradesController {
+
+}
