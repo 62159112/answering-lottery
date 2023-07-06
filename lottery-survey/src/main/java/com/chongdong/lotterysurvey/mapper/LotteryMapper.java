8aeb48801712770ae805e6db02885c26c2253773
黄煜龙 <534411514@qq.com>
2023-07-06 11:35:51 +0800

    添加奖品类相关mybatisplus生成类，添加抽奖控制层，添加获取抽奖方法

---
+++ lottery-survey/src/main/java/com/chongdong/lotterysurvey/mapper/LotteryMapper.java
@@ -0,0 +1,18 @@
+package com.chongdong.lotterysurvey.mapper;
+
+import com.chongdong.lotterysurvey.model.Lottery;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author cd
+* @description 针对表【tcd_lottery】的数据库操作Mapper
+* @createDate 2023-07-06 11:30:52
+* @Entity com.chongdong.lotterysurvey.model.Lottery
+*/
+public interface LotteryMapper extends BaseMapper<Lottery> {
+
+}
+
+
+
+
---
+++ lottery-survey/src/main/java/com/chongdong/lotterysurvey/service/LotteryService.java
@@ -0,0 +1,14 @@
+package com.chongdong.lotterysurvey.service;
+
+import com.chongdong.lotterysurvey.model.Lottery;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.chongdong.lotterysurvey.model.ResponseMap;
+
+/**
+* @author cd
+* @description 针对表【tcd_lottery】的数据库操作Service
+* @createDate 2023-07-06 11:30:52
+*/
+public interface LotteryService extends IService<Lottery> {
+    ResponseMap getResult(Integer userId);
+}
