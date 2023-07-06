4e0ad40cc57b5445707fc9bb47f3966c048e726e
黄煜龙 <534411514@qq.com>
2023-07-06 08:54:35 +0800

    项目初始化

---
+++ lottery-survey/src/main/java/com/chongdong/lotterysurvey/LotterySurveyApplication.java
@@ -0,0 +1,13 @@
+package com.chongdong.lotterysurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotterySurveyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotterySurveyApplication.class, args);
+    }
+
+}
