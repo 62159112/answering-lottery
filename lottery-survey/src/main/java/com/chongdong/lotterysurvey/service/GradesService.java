c6e5010aa6f89ddc9115c847388dfb7bdf5312d5
Pip190 <wjk152153431@outlook.com>
2023-07-06 10:40:30 +0800

    新增：个人成绩表、团队答题人次表

---
+++ lottery-survey/src/main/java/com/chongdong/lotterysurvey/service/GradesService.java
@@ -0,0 +1,13 @@
+package com.chongdong.lotterysurvey.service;
+
+import com.chongdong.lotterysurvey.model.Grades;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author wo
+* @description 针对表【tcd_grades(成绩表（个人成绩）)】的数据库操作Service
+* @createDate 2023-07-06 10:37:43
+*/
+public interface GradesService extends IService<Grades> {
+
+}
