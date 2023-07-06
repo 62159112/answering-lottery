e10665411caa4cedfffcd93768f5ca692388e30d
yanghang <1339160008@qq.com>
2023-07-06 14:57:46 +0800

    生成

---
+++ lottery-survey/src/main/java/com/chongdong/lotterysurvey/mapper/AnswerMapper.java
@@ -0,0 +1,18 @@
+package com.chongdong.lotterysurvey.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chongdong.lotterysurvey.model.Answer;
+
+/**
+* @author cd
+* @description 针对表【tcd_answer(答题表)】的数据库操作Mapper
+* @createDate 2023-07-06 14:50:26
+* @Entity com.chongdong.lotterysurvery.model.Answer
+*/
+public interface AnswerMapper extends BaseMapper<Answer> {
+
+}
+
+
+
+
---
+++ lottery-survey/src/main/java/com/chongdong/lotterysurvey/service/impl/AnswerServiceImpl.java
@@ -0,0 +1,23 @@
+package com.chongdong.lotterysurvey.service.impl;
+
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.chongdong.lotterysurvey.mapper.AnswerMapper;
+import com.chongdong.lotterysurvey.model.Answer;
+import com.chongdong.lotterysurvey.service.AnswerService;
+import org.springframework.stereotype.Service;
+
+/**
+* @author cd
+* @description 针对表【tcd_answer(答题表)】的数据库操作Service实现
+* @createDate 2023-07-06 14:50:26
+*/
+@Service
+public class AnswerServiceImpl extends ServiceImpl<AnswerMapper, Answer>
+    implements AnswerService {
+
+}
+
+
+
+
